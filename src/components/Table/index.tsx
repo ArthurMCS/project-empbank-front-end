51bc43abfc2f1e333676e27b9df8f69501586222
Arthur Moreira <arthurmcs96@gmail.com>
2023-02-14 13:00:54 -0300

    creates the table component styles

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,30 +1,37 @@
 import React, { useContext } from 'react'
-import { Table } from '@mantine/core'
+import { ScrollArea, Table } from '@mantine/core'
 import { useStyles } from './styles';
 import { context } from '../../context';
 
 export default function TransactionsTable() {
     const {transactions} = useContext(context);
+    const { classes } = useStyles()
 
     const rows = transactions.map((transaction) => (
-        <tr key={transaction.id}>
-          <td>{transaction.title}</td>
-          <td>{transaction.value}</td>
-          <td>{transaction.category}</td>
-          <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.created_at))}</td>
+        <tr key={transaction.id} className={classes.tr}>
+          <td className={classes.title}>
+            <div className={classes.circle} style={{ backgroundColor: transaction.cashIn ? 'green' : 'red'}}/>
+            {transaction.title}
+         </td>
+          <td className={classes.td} style={{ color: transaction.cashIn ? 'green' : 'red'}}>
+            {transaction.cashIn ? `R$ ${transaction.value}` : `-R$ ${transaction.value}`}
+         </td>
+          <td className={classes.td}>{transaction.category}</td>
+          <td className={classes.td}>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.created_at))}</td>
         </tr>
     ));  
 
     return (
-        <Table
-            mt="25px"
-            horizontalSpacing="xl" 
-            verticalSpacing="lg" 
-            fontSize="xs"
-        >
-           <tbody>
-                {rows}
-           </tbody>
-        </Table>
+        <div className={classes.container}>
+            <table
+                className={classes.table}
+            >
+                <tbody
+                    className={classes.body}
+                >
+                        {rows}
+                </tbody>
+            </table>     
+        </div>
     )
 }
--- src/components/Table/styles.ts
+++ src/components/Table/styles.ts
@@ -1,13 +1,45 @@
 import { createStyles } from '@mantine/core';
 
-export const useStyles = createStyles((theme) => ({
-   /*  tr: {
+export const useStyles = createStyles(() => ({
+    container: {
+        height: 409,
+        overflowY: 'auto',
+        '&::-webkit-scrollbar': { 
+            display: 'none',
+        },
+        marginTop: 20,
+    },
+    table: {
+        marginTop: 10,
+        width: '100%',
+        borderSpacing: '0 0.5rem',
+    },
+    body: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 16,
+    },
+    tr: {
+        border: '1px solid #CED4DA',
+        borderRadius: 5,
+        height: 56,
+        padding: '20px, 18px, 20px, 18px',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-around',
-        borderRadius: '5px',
-        border: '1px solid #CED4DA',
-        marginBottom: '16px',
-        height: 66,
-    }, */
+    },
+    td: {
+        textAlign: 'center',
+        width: '100%',
+    },
+    title: {
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+    },
+    circle: {
+        borderRadius: '100%',
+        height: 15,
+        width: 15,
+        margin: '0px 25px 0px 15px',
+    }
 }));
